f0299700aa15ede7d3bedbf289b0eed1875c8c9f
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-08 23:09:22 +0530

    [Pooja] added: EmailNotification class

---
+++ EmailNotification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patttern_Practice
+{
+    public class EmailNotification : INotification
+    {
+        public void GetNotify(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class SMSNotification : INotification
+    {
+        public void GetNotify(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class PushNotification : INotification
+    {
+        public void GetNotify(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}

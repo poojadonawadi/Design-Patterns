24bc776f730f2a70bce851e45117ccb3d36278d1
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-08 23:05:04 +0530

    [Pooja] added: Shape Factory class

---
+++ ShapeFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patttern_Practice
+{
+    internal class ShapeFactory
+    {
+        public IShape GetShape(string shapeType)
+        {
+            if (shapeType == null)
+            {
+                return null;
+            }
+            else if(shapeType == "Circle")
+            {
+                return new Circle();
+            }
+            else if (shapeType == "Rectangle")
+            {
+                return new Rectangle();
+            }
+            return null;
+        }
+    }
+}

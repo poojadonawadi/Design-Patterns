a4fb2b8ee056161163f33c2a353e949ebaf1ea34
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-08 23:12:48 +0530

    [Pooja] added: Singleton class

---
+++ Singleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patttern_Practice
+{
+    //sealed keyword is used to avoid the multiple instantiation by the inherited nested classes
+    //which voilates the Singleton rules
+    internal sealed class Singleton
+    {
+        private static Singleton Instance = null;
+        private static readonly object lockObject = new object();
+        private Singleton() { }
+
+        //SIngle threaded environment
+        //public static Singleton GetInstance
+        //{
+        //    get
+        //    {
+        //        if(Instance == null)
+        //        {
+        //            Instance = new Singleton();
+        //        }
+        //        return Instance;
+        //    }
+
+        //}
+
+        //Multithreaded environment
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    lock (lockObject)
+                    {
+                        if (Instance == null)
+                        {
+                            Instance = new Singleton();
+                        }
+                    }
+                }
+                return Instance;
+            }
+        }
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}

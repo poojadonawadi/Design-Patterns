8805be59c74fb83d92e311930c2c0e3565d2b520
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-08 23:02:38 +0530

    [Pooja] added circle class

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patttern_Practice
+{
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Circle");
+        }
+    }
+
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing Rectangle");
+        }
+    }
+}
